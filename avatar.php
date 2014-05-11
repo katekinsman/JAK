1aa5a2c139161fef018d5652227493aa45295539
ashleyjade <ashleyjade@outlook.com>
2014-05-10 21:22:03 -0700

    Updated My Avatar and Play pages

--- avatar.php
+++ avatar.php
@@ -1,1 +1,117 @@
-<p>Check out your avatar here!</p>
+<div class="panel panel-default">
+  <div class="panel-body">
+  	<h1>My Avatar</h1>
+  	<h2>You have # coins!</h2>
+    
+	<div id="myCarousel" class="carousel slide" data-interval="false">
+
+	  <!-- Carousel items -->
+	  <div class="carousel-inner">
+	  	<img src="http://www.clipartbest.com/cliparts/nTB/X6B/nTBX6BETA.gif" height="10%" width="10%">
+	    <h3>Inventory</h3>
+	    
+	    <div class="active item">
+	    	<table class="table">
+                <tr>
+                    <td><a href="#" class="thumbnail">
+				         <img src="http://placehold.it/140x100" 
+				         alt="Generic placeholder thumbnail">
+				      </a></td>
+					<td><a href="#" class="thumbnail">
+				         <img src="http://placehold.it/140x100" 
+				         alt="Generic placeholder thumbnail">
+				      </a></td>
+					<td><a href="#" class="thumbnail">
+				         <img src="http://placehold.it/140x100" 
+				         alt="Generic placeholder thumbnail">
+				      </a></td>
+                </tr>
+                <tr>
+                   	<td><a href="#" class="thumbnail">
+				         <img src="http://placehold.it/140x100" 
+				         alt="Generic placeholder thumbnail">
+				      </a></td>
+                   	<td><a href="#" class="thumbnail">
+				         <img src="http://placehold.it/140x100" 
+				         alt="Generic placeholder thumbnail">
+				      </a></td>
+                   	<td><a href="#" class="thumbnail">
+				         <img src="http://placehold.it/140x100" 
+				         alt="Generic placeholder thumbnail">
+				      </a></td>
+                </tr>
+                <tr>
+                   	<td><a href="#" class="thumbnail">
+				         <img src="http://placehold.it/140x100" 
+				         alt="Generic placeholder thumbnail">
+				      </a></td>
+                   	<td><a href="#" class="thumbnail">
+				         <img src="http://placehold.it/140x100" 
+				         alt="Generic placeholder thumbnail">
+				      </a></td>
+                   	<td><a href="#" class="thumbnail">
+				         <img src="http://placehold.it/140x100" 
+				         alt="Generic placeholder thumbnail">
+				      </a></td>
+                </tr>
+            </table>
+
+		</div>
+	    <div class="item">
+	    		    	<table class="table">
+                <tr>
+                    <td><a href="#" class="thumbnail">
+				         <img src="http://placehold.it/140x100" 
+				         alt="Generic placeholder thumbnail">
+				      </a></td>
+					<td><a href="#" class="thumbnail">
+				         <img src="http://placehold.it/140x100" 
+				         alt="Generic placeholder thumbnail">
+				      </a></td>
+					<td><a href="#" class="thumbnail">
+				         <img src="http://placehold.it/140x100" 
+				         alt="Generic placeholder thumbnail">
+				      </a></td>
+                </tr>
+                <tr>
+                   	<td><a href="#" class="thumbnail">
+				         <img src="http://placehold.it/140x100" 
+				         alt="Generic placeholder thumbnail">
+				      </a></td>
+                   	<td><a href="#" class="thumbnail">
+				         <img src="http://placehold.it/140x100" 
+				         alt="Generic placeholder thumbnail">
+				      </a></td>
+                   	<td><a href="#" class="thumbnail">
+				         <img src="http://placehold.it/140x100" 
+				         alt="Generic placeholder thumbnail">
+				      </a></td>
+                </tr>
+                <tr>
+                   	<td><a href="#" class="thumbnail">
+				         <img src="http://placehold.it/140x100" 
+				         alt="Generic placeholder thumbnail">
+				      </a></td>
+                   	<td><a href="#" class="thumbnail">
+				         <img src="http://placehold.it/140x100" 
+				         alt="Generic placeholder thumbnail">
+				      </a></td>
+                   	<td><a href="#" class="thumbnail">
+				         <img src="http://placehold.it/140x100" 
+				         alt="Generic placeholder thumbnail">
+				      </a></td>
+                </tr>
+            </table>
+		</div>
+
+	  </div>
+	  <!-- Carousel nav -->
+    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+        <span class="glyphicon glyphicon-chevron-left"></span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" data-slide="next">
+        <span class="glyphicon glyphicon-chevron-right"></span>
+	</div>
+
+  </div>
+</div>
--- play.php
+++ play.php
@@ -1,1 +1,103 @@
-<P>Yay, play games here!</p>
+<div class="panel panel-default">
+  <div class="panel-body">
+    
+	<div id="myCarousel" class="carousel slide" data-interval="false">
+
+	  <!-- Carousel items -->
+	  <div class="carousel-inner">
+	    <div class="active item">
+	    	<div class="panel panel-default">
+		    	<h1>Story Title</h1>
+		    	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		    	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		    	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+		    	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+		    	cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+		    	proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+	    	</div>
+    	</div>
+	    <div class="item">
+	    	<div class="panel panel-default">
+			    <h1>Page One</h1>
+		    	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		    	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		    	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+		    	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+		    	cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+		    	proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+	    	</div>
+	    </div>
+	    <div class="item">
+	    	<div class="panel panel-default">
+		    	<h1>Page Two</h1>
+		    	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		    	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		    	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+		    	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+		    	cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+		    	proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+		    </div>
+		</div>
+		<div class="item">
+	    	<div class="panel panel-default">
+		    	<h1>Question One</h1>
+		    	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		    	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		    	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+		    	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+		    	cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+		    	proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+		    	<form>
+					<input type="radio" name="option" value="1">One<br>
+					<input type="radio" name="option" value="2">Two<br>
+					<input type="radio" name="option" value="3">Three<br>
+					<input type="radio" name="option" value="4">Four
+				</form>
+		    </div>
+		</div>
+		<div class="item">
+	    	<div class="panel panel-default">
+		    	<h1>Question Two</h1>
+		    	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		    	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		    	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+		    	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+		    	cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+		    	proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+		    	<form>
+					<input type="radio" name="option" value="1">One<br>
+					<input type="radio" name="option" value="2">Two<br>
+					<input type="radio" name="option" value="3">Three<br>
+					<input type="radio" name="option" value="4">Four
+				</form>
+		    </div>
+		</div>
+		<div class="item">
+	    	<div class="panel panel-default">
+		    	<h1>Question Three</h1>
+		    	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		    	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		    	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+		    	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+		    	cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+		    	proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+		    	<form>
+					<input type="radio" name="option" value="1">One<br>
+					<input type="radio" name="option" value="2">Two<br>
+					<input type="radio" name="option" value="3">Three<br>
+					<input type="radio" name="option" value="4">Four
+				</form>
+		    </div>
+		</div>
+
+	  </div>
+	  <!-- Carousel nav -->
+    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+        <span class="glyphicon glyphicon-chevron-left"></span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" data-slide="next">
+        <span class="glyphicon glyphicon-chevron-right"></span>
+	</div>
+
+  </div>
+</div>
