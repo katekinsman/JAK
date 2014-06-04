5086d66e5acf589eb78aea3df14f5a5435ffad42
ashleyjade <ashleyjade@outlook.com>
2014-06-04 15:55:29 -0700

    Results page updated
    Includes lake image and better set of messages depending on coins
    earned. Also added CSS to student landing.

--- studentCoins.php
+++ studentCoins.php
@@ -16,17 +16,31 @@
 
     $theme = $_REQUEST["theme"];
 
+    // Query for coins
 	$coinsquery = ("SELECT `StorySum` FROM `vw_totalstorycoins`
 		WHERE `StudentName` = '$user'
 		AND `Theme` = '$theme'");
 	$result = $db->query($coinsquery);
 	$coins = $result->fetch(PDO::FETCH_BOTH);
 
-	echo "You found $coins[0] coins! ";
+    // Query for obstacle information
+    $obstacleQuery = "SELECT Message, MapImage FROM `vw_fulljourney`
+        WHERE `Theme` = '$theme' AND `Stage` = 4";
+    $obstacleResult = $db->query($obstacleQuery);
+    $result2 = $obstacleResult->fetch(PDO::FETCH_BOTH);
 
     if ($coins[0] > 13) {
-        echo "You were outstanding!";
+        echo "<h1>You were outstanding!</h1><br>";
+        echo "<p>Good job, " . $result2['Message'] . ". You also found $coins[0] coins!</p><br>";
+        echo "<img src='" . $result2['MapImage'] . "' height='40%' width='40%' style='margin: 0 auto 0 auto'/>";
+    } else if ($coins[0] < 0) {
+        echo "<h1>Keep working!</h1><br>";
+        echo "<p>Good job, " . $result2['Message'] . ". But you lost $coins[0] coins.</p><br>";
+        echo "<img src='" . $result2['MapImage'] . "' height='40%' width='40%' style='margin: 0 auto 0 auto'/>";
     } else {
-        echo "Keep working!";
+        echo "<h1>Good job!</h1><br>";
+        echo "<p>Well done, " . $result2['Message'] . ". You also found $coins[0] coins!</p><br>";
+        echo "<img src='" . $result2['MapImage'] . "' height='40%' width='40%' style='margin: 0 auto 0 auto'/>";
     }
+
 ?>
--- studentlanding.php
+++ studentlanding.php
@@ -1,9 +1,30 @@
-<div class="jumbotron" style="text-align:center; background-color:#E5DBC1">
-    <h2>Hello, <?php print $user ?>!</h2>
-    <p>What would you like to do?</p>
-    <div class="btn-group-vertical btn-group-lg">
-        <a class="btn btn-default" href="/studentTemplate.php?page=theme"><span class="glyphicon glyphicon-play"></span> Play</a>
-        <a class="btn btn-default" href="/studentTemplate.php?page=avatar"><span class="glyphicon glyphicon-user"></span> View my Avatar</a>
-        <a class="btn btn-default" href="/studentTemplate.php?page=store"><span class="glyphicon glyphicon-shopping-cart"></span> Visit the Store</a>
-    </div>
-</div>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+        <!-- Compiled and minified CSS -->
+        <link rel="stylesheet" href="bootstrap-3.1.1-dist/css/bootstrap.min.css">
+        <!-- Optional theme -->
+        <link rel="stylesheet" href="bootstrap-3.1.1-dist/css/bootstrap-theme.min.css">
+
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+        <!-- Include all compiled plugins (below), or include individual files as needed -->
+        <script src="bootstrap-3.1.1-dist/js/bootstrap.min.js"></script>
+        
+		<title>Student Landing</title>
+	</head>
+
+	<body style="background-color:#E5DBC1">
+		<div class="jumbotron" style="text-align:center; background-color:#E5DBC1">
+		    <h2>Hello, <?php print $user ?>!</h2>
+		    <p>What would you like to do?</p>
+		    <div class="btn-group-vertical btn-group-lg">
+		        <a class="btn btn-default" href="/studentTemplate.php?page=theme"><span class="glyphicon glyphicon-play"></span> Play</a>
+		        <a class="btn btn-default" href="/studentTemplate.php?page=avatar"><span class="glyphicon glyphicon-user"></span> View my Avatar</a>
+		        <a class="btn btn-default" href="/studentTemplate.php?page=store"><span class="glyphicon glyphicon-shopping-cart"></span> Visit the Store</a>
+		    </div>
+		</div>
+	</body>
+
+</html>
